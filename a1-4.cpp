1d6bba77471c2af37c154d78cfe1d0fa7b54a285
aarya165 <150890580+aarya165@users.noreply.github.com>
2024-09-27 11:18:58 +0530

    Update a1-4.cpp

--- a1-4.cpp
+++ a1-4.cpp
@@ -6,8 +6,34 @@ using namespace std;
 
 // Function to find the median
 double findMedian(int arr1[], int size1, int arr2[], int size2) {
+    int totalSize = size1 + size2;
+    int medianPos = totalSize / 2;
+    bool isEven = (totalSize % 2 == 0); // here i am checking if we have even or odd in total number of elements                                                                                
     
-// Your code here
+    int i = 0, j = 0; // starting for the two arrays
+    int current, previous;
+    
+    // Mergring the arrays to reach median
+    for (int count = 0; count <= medianPos; count++) {
+        previous = current;
+        
+        //this is to compare both the values and change the i and j values accordingly
+        if (i < size1 && (j >= size2 || arr1[i] < arr2[j])) {
+            current = arr1[i];
+            i++;
+        } else {
+            current = arr2[j];
+            j++;
+        }
+    }
+    
+    // If the total number of elements is even, we need to return the average of two middle elements
+    if (isEven) {
+        return (current + previous) / 2.0;
+    }
+    
+    // If the total number of elements is odd, just return the current element
+    return current;
 
 }
 
